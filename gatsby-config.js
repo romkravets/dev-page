55606c6507a9f382dac3fbe73254d4af53fe7bb1
romkravets <romann.kravets@gmail.com>
2019-09-21 14:05:26 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/https://github.com/romkravets/dev-page.git",
   siteMetadata: {
     title: `Rom Kravets`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
