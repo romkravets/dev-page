ffdbebbc4ed6c62f9e8492200adbe7bdea2ad2e9
romkravets <romann.kravets@gmail.com>
2019-10-13 18:14:08 +0300

    target link

--- src/components/SocIcons.js
+++ src/components/SocIcons.js
@@ -8,27 +8,27 @@ import { FaTelegramPlane } from "react-icons/fa"
 
 const SocIcons = () => (
   <div className="home__icons">
-    <a className="home__link" href="https://www.facebook.com/romann.kravets">
+    <a className="home__link" href="https://www.facebook.com/romann.kravets" target="_blank" rel="noopener noreferrer">
       <i className="fab fa-twitter fa-2x">
         <FaFacebookF />
       </i>
     </a>
-    <a className="home__link" href="mailto:romann.kravets@gmail.com">
+    <a className="home__link" href="mailto:romann.kravets@gmail.com" target="_blank" rel="noopener noreferrer">
       <i className="fab fa-facebook fa-2x">
         <IoIosMail />
       </i>
     </a>
-    <a className="home__link" href="https://github.com/romkravets">
+    <a className="home__link" href="https://github.com/romkravets" target="_blank" rel="noopener noreferrer">
       <i className="fab fa-linkedin fa-2x">
         <FaGithub />
       </i>
     </a>
-    <a className="home__link" href="https://www.linkedin.com/in/romkravets">
+    <a className="home__link" href="https://www.linkedin.com/in/romkravets" target="_blank" rel="noopener noreferrer">
       <i className="fab fa-github fa-2x">
         <FaLinkedinIn />
       </i>
     </a>
-    <a className="home__link" href=" https://t.me/romkravets">
+    <a className="home__link" href=" https://t.me/romkravets" target="_blank" rel="noopener noreferrer">
       <i className="fab fa-github fa-2x">
         <FaTelegramPlane />
       </i>
